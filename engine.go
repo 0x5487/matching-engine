b1fe3ca4af240c0ffb8d8adeb4ff8bb7d18b93c8
0x5487 <jawc@hotmail.com>
2025-12-13 06:28:19 +0000

    - refactor: replace `Trade` with `BookLog` to capture Open, Match, and Cancel events for full order book reconstruction

--- engine.go
+++ engine.go
@@ -10,22 +10,26 @@ type MatchingEngine struct {
 	publishTrader PublishTrader
 }
 
+// NewMatchingEngine creates a new matching engine instance.
 func NewMatchingEngine(publishTrader PublishTrader) *MatchingEngine {
 	return &MatchingEngine{
 		publishTrader: publishTrader,
 	}
 }
 
+// AddOrder adds an order to the appropriate order book based on the market ID.
 func (engine *MatchingEngine) AddOrder(ctx context.Context, order *Order) error {
 	orderbook := engine.OrderBook(order.MarketID)
 	return orderbook.AddOrder(ctx, order)
 }
 
+// CancelOrder cancels an order in the appropriate order book.
 func (engine *MatchingEngine) CancelOrder(ctx context.Context, marketID string, orderID string) error {
 	orderbook := engine.OrderBook(marketID)
 	return orderbook.CancelOrder(ctx, orderID)
 }
 
+// OrderBook retrieves the order book for a specific market ID, creating it if it doesn't exist.
 func (engine *MatchingEngine) OrderBook(marketID string) *OrderBook {
 	book, found := engine.orderbooks.Load(marketID)
 	if !found {
